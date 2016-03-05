b79845056b44a724ec82d148dd56774080a29bab
Jhaybee Urquia <Jhaybee Urquia>
2016-03-05 11:58:50 +0800

    Finals-Quiz1

---
+++ SampleEncrypt/EncryptingText.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SampleEncrypt
+{
+    partial class EncryptingText
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StringBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EncryptedBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(20, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(20, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Encrypted";
+            // 
+            // StringBox
+            // 
+            this.StringBox.Location = new System.Drawing.Point(98, 97);
+            this.StringBox.Name = "StringBox";
+            this.StringBox.Size = new System.Drawing.Size(249, 23);
+            this.StringBox.TabIndex = 2;
+            this.StringBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(92, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Encrypting Text";
+            // 
+            // EncryptedBox
+            // 
+            this.EncryptedBox.Location = new System.Drawing.Point(98, 239);
+            this.EncryptedBox.Name = "EncryptedBox";
+            this.EncryptedBox.Size = new System.Drawing.Size(249, 23);
+            this.EncryptedBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(147, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // EncryptingText
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuText;
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EncryptedBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.StringBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EncryptingText";
+            this.Text = "Encrypt Sample Vargas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox StringBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox EncryptedBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SampleEncrypt/EncryptingText.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleEncrypt
+{
+    public partial class EncryptingText : Form
+    {
+        public EncryptingText()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((!string.IsNullOrWhiteSpace(StringBox.Text) && !string.IsNullOrWhiteSpace(StringBox.Text)) == true)
+            {
+                try
+                {
+                    EncryptedBox.Text = EncryptClass.EncryptString(StringBox.Text, StringBox.Text);
+                    StringBox.Text = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
